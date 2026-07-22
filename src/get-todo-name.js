ff767f83259319945215ae2164f6a4a8d4886807
henthaii <200550625+henthaii@users.noreply.github.com>
2026-07-21 22:47:10 -0500

    added new function to start adding todo testing priority with get-todo-name new-todo had to update priority name

--- src/get-todo-name.js
+++ src/get-todo-name.js
@@ -4,14 +4,18 @@
 // return can only return one value but can do arrays
 // need to figure out how to add to an array
 // do i need to make a new function to create array holders
+// for now, get class to work with this and append to end of whatever element is left
 
+import {addTodoToProject} from "./addTodoToProject.js"
 
 function getTodoName(formElement) {
     const formData = new FormData(formElement);
-    return formData.get("title"),
-    formData.get("description"),
-    formData.get("due-date"),
-    formData.get("priority-level"),
-}
+    // addTodoToProject(
+        // return formData.get("title")
+        // formData.get("description"),
+        // formData.get("due-date"),
+        return formData.get("priority")
+    // );
+};
 
 export {getTodoName}
